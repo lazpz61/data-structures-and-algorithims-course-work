226a79e2437d7b2a8dce2a9dc7c9e9071f3607d2
LazaroMPerez <lazpz61@gmail.com>
2021-05-25 01:01:50 -0400

    big o of objects

---
+++ 1-Big-O-Notation/12.Logs-and-Section-Recap.js
@@ -0,0 +1,36 @@
+// What is a log
+
+// it is the inverse of an exponential equation
+
+log2 (8) = 3
+
+
+/* The algorithm of a number roughly measure the number of times you can divide that number by 2
+before you get a value thats less than or equal to 1.
+*/
+
+
+// O(log n) time complexity is good. it compares very favoarably like O(n) 
+
+// O(nlogn) is good but not as good as O(log n) time complexity. 
+
+
+// Who Cares 
+
+// - Search algorithms, and sorting algorithms, recursion sometimes invloves logarithmic space complexity. 
+
+// Recap
+
+/*
+
+- to analyze performance of an algorithm we use Big O Notation. 
+
+- Big O Notation can give us a high level understanding of the time or space complexity of an algorithm.
+
+- Big O Notation doesnt care about precision, only about feneral trends ( linear, quadratic, constant)
+
+- The time or space complexity (as measure by Big O) depends only on the algorithm, 
+not the hardware used to run the algorithm. 
+
+*/
+
