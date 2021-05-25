0158cc7113e50f8490d6b0574f040d129ba5d214
LazaroMPerez <lazpz61@gmail.com>
2021-05-25 00:14:35 -0400

    big o notes

---
+++ Big-O-Notation/10.Space-Complexity.js
@@ -0,0 +1,30 @@
+// Time complexity is the runtime.
+
+// Space Complexity =  how much additional memory do we need to allocate in order to run the code in oiur algorithim.
+
+// Aucxiliary Space Complexity refers to space required by the algorithm, not including space taken up be the inputs.Aucxiliary
+
+// most Primitives (booleans, numbers, undefined, null) are constant space. 
+
+// Strings require O(n) space where n is the string length.toExponential
+
+// reference types are generally O(n), where n is the length (for arrays) or the number of keys (for objects)
+
+function sum(arr) {
+    let total = 0;
+    for (let i = 0; i <arr.length; i++) {
+    total +=arr[i];
+    }
+    return total;
+}
+
+// O(1) -  this is the space of the function above
+
+function double(arr) {
+    let newArr = [];
+    for (let i = 0; i< arr.length; i++) {
+        newArr.push(2 * arr[i]);
+    }
+    return newArr;
+
+}
---
+++ Big-O-Notation/6-simplifyingBigOExpressions.js
@@ -0,0 +1,58 @@
+// // When determining the time complexity of an algorithm, there are some helpful
+// rules of thumb for big O expressions. 
+
+/* Constants Dont Matter
+
+O(2n) is O(n)
+
+O(500) is O(1)
+
+
+O(13n^2)  is O(n^2)
+
+*/
+
+/* smaller terms dont matter
+
+O(n+10) can be simplified to O(n)
+
+O(1000n +50) can be simplified with O(n)
+
+O(n^2 + 5n +8) can be simplified with O(n^2)
+
+
+
+
+*/
+
+
+
+/*
+
+Big O Shorthands
+
+1) Arithmetic operations are constant.
+2) Variable assignment is constant. 
+3) Accessing elemnts in an array (by index) or object (by key) is constant 
+4) In a loop, the complexity is constant length of the loop times the complexity of whatever happens inside of the loop.
+
+*/
+
+
+function logAtLeast5(n) {
+    for (let i = 1; i <= Math.max(5, n); i++) {
+        console.log(i);
+    }
+}
+
+// this is going is going to be O(n)
+
+function logAtMost5(n) {
+    for (let i =1; i <= Math.min(5,n); i++) {
+        console.log(i);
+    }
+}
+
+// This is going to O(1)
+
+// O(1) is the fastst runtime then O(n) is second then O(n^2) is slower than O(n).
