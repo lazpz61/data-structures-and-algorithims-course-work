47cedf8f715111e9c43541b86d790d5822108585
LazaroMPerez <lazpz61@gmail.com>
2021-05-26 15:36:04 -0400

    breaking down problems

---
+++ 3-Problem-Solving-Approach/20.Conrete-Examples.js
@@ -0,0 +1,39 @@
+// Exploring conrete examples
+
+/*
+
+1. Start with simple examples.
+2. Progress to More complex Examples.
+3. Explore Examples with Empty Inputs.
+4. Explore examples with invalid inputs. 
+
+*/
+
+
+
+
+
+// come up with concrete eamples, it can help you understnad the problem better, it provides sanity checks.
+
+// if you have an eventual solution you can check your work.
+
+// user stories - given an input what is our output.  
+
+// Unit tests help layout how it should work.
+
+
+
+
+// writing down 3 simple examples will help you understand to where you want to get in the challenge. 
+
+
+// Write a function which takes in a string and returns counts of each character in the string
+
+
+charCount("aaaa"); // {a:4}
+charCount("hello");// {h:1, e:1, l:2, o:1}
+
+"my phone number is 12870"
+
+"Hello hi"
+charCount("");
---
+++ 3-Problem-Solving-Approach/21.Break-It-Down.js
@@ -0,0 +1,33 @@
+// Break it down before coding. Take the steps and write down what I need to write out. 
+
+// You should communicate what you are doing while your coding in front of the interviewer.
+
+// Write pseudo code so you write steps by step to do what your doing. 
+
+charCount("aaaa"); // {a:4}
+charCount("hello");// {h:1, e:1, l:2, o:1}
+
+"my phone number is 12870"
+
+"Hello hi"
+charCount("");
+
+
+functuon charCount(str) {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string;
+}
+
+
+functuon charCount(str) {
+// make object to return at end
+// loop over string, for each character...
+// if the char is a number/letter AND key in object, add 1 to count
+// if character is a number/letter AND not in object, add it and set value to 1
+// if character is something else( sace, period, etc) dont do anything 
+
+// return object at end
+}
+
+// The interveiw is really about seeing your process to solving the problem at hand. 
+
