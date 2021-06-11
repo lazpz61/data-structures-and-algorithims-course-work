67da9337271240e033fa02c72768cd9ebebf090b
LazaroMPerez <lazpz61@gmail.com>
2021-06-11 01:29:15 -0400

    sliding window patterns

--- 4-Problem-Solving-Patterns/28.Anagram-Challenge.js
+++ 4-Problem-Solving-Patterns/28.Anagram-Challenge.js
@@ -40,7 +40,7 @@ function validAnagram(first, second) {
     for (let i= 0; i <first.length; i++){
         let letter = first[i];
         // if letter exists, incremenet, otherwise set to 1
-        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1; // Ternary Operator
     }
     for (let i =0; i <second.length; i++){
         let letter = second[i];
@@ -56,7 +56,7 @@ function validAnagram(first, second) {
 
 
 /* Anytime we have multiple pieces of data and we need to compare them, and
-you need to see if they consists of the same individual pieces, if one arraty is equal to anothe rarray squared
+you need to see if they consists of the same individual pieces, if one array is equal to another array squared
 the orders of the applications matter as well. 
 
 */
