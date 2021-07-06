37125e03311046d30b996049e433d73d78a7a781
LazaroMPerez <lazpz61@gmail.com>
2021-07-06 13:34:10 -0400

    Radix Sort algorithm

--- 16.Radix-Sort/100.Radix-Sort-Helper-Methods.js
+++ 16.Radix-Sort/100.Radix-Sort-Helper-Methods.js
@@ -1,14 +1,45 @@
+/*
+
+Radix Sort needs a few helper functions first.  
+
+The get digit helper function will get us the digit in place
+This returns the digit in num at the given place value. 
+
+*/
+
+
+
+
+
 // The below is the radix sort helper method 
 
 
 
-function getDigit(num, i) {
-    return Math.floor(Math.abs(num) / Math.pow(10,i)) % 10;
+function getDigit(number, index) {
+    return Math.floor(Math.abs(number) / Math.pow(10,index)) % 10;
 }
 
-function digitCount(num) {
-    if (num === 0) return 1;
-    return Math.floor(Math.log10(Math.abs(num)))+1;
+/*
+Now what we'll need a is a count for what is the amount of digits for the highest valued number. 
+
+
+*/
+
+function digitCount(number) {
+    if (number === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(number)))+1;
 }
 
-getDigit(7323,2);
+/*
+Most Digits : given an array of number, returns the number of digits in the largest numbers in the list.
+*/
+
+function mostDigits(numbers){
+    let maximumDigits = 0;
+    for (let iteratedNum = 0; iteratedNum < numbers.length; iteratedNum++){
+        maximumDigits = Math.max(maximumDigits, digitCount(numbers[iteratedNum]));
+    }
+    return maximumDigits;
+}
+
+
---
+++ 16.Radix-Sort/102.Radix-Sort-Implementation.js
@@ -0,0 +1,33 @@
+// Helper Methods 
+
+function getDigit(number, index) {
+    return Math.floor(Math.abs(number) / Math.pow(10,index)) % 10;
+}
+
+function digitCount(number) {
+    if (number === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(number)))+1;
+}
+
+function mostDigits(numbers){
+    let maximumDigits = 0;
+    for (let iteratedNum = 0; iteratedNum < numbers.length; iteratedNum++){
+        maximumDigits = Math.max(maximumDigits, digitCount(numbers[iteratedNum]));
+    }
+    return maximumDigits;
+}
+
+function radixSort(numbers){
+    let maxDigitCount = mostDigits(numbers);
+    for (let iteratedIndex =0; iteratedIndex < maxDigitCount; iteratedIndex++){
+        let digitBuckets = Array.from({length: 10},  () => []);
+        for (let value = 0; value < numbers.length; value++){
+            let digit = getDigit(numbers[value], iteratedIndex)
+            digitBuckets[digit].push(numbers[value]);
+        }
+        numbers = [].concat(...digitBuckets);
+    }
+    return numbers;
+}
+
+radixSort([23,345,5467,12,2345,9852])
