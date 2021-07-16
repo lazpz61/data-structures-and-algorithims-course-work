714dd7f7c4e8eaab3e6e3f6a67cfcc93639db14d
LazaroMPerez <lazpz61@gmail.com>
2021-07-16 01:43:23 -0400

    singly linked list pop

---
+++ 18.Singly-Linked-Lists/110.Singly-Linked-Lists.js
@@ -0,0 +1,48 @@
+/*
+
+What is a Linked List?
+
+Data structure that store data and order, and it contains a head, tail and length property.
+
+A Linked list consists of nodes, and each node has a value and a pointer to another node or null. 
+
+Nodes stores data, and references the next node, or if theres nothing left. Head is beginning and the tail is the end
+
+keeping track of length to keep track of things.
+
+a bunch of nodes pointing to another a chain of data points the points to another point of data. 
+
+*/
+
+
+class Node{
+    constructor(value){
+        this.value = value,
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+
+    }
+}
+
+let list = new SinglyLinkedList()
+list.push("Hello")
+list.push("Goodbye")
---
+++ 18.Singly-Linked-Lists/114.Singly-Linked-List-Pop-Solution.js
@@ -0,0 +1,57 @@
+/*
+Popping - is removing from the end of a linked list. 
+
+Popping Pseudocode 
+
+-If there are no nodes in the lift, return undefined.
+-Loop through the list until you reach the tail.
+-Set the next property of the 2nd to last node to be null 
+- Set the tail to be the 2nd to last node
+-Decrement the lneght of the list by 1
+-Return the value of the node removed
+*/
+
+class Node{
+    constructor(value){
+        this.value = value,
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+
+    }
+    pop(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
