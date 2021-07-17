b32cdd089bb65f3ce09369e96ff30a55506cf2f2
LazaroMPerez <lazpz61@gmail.com>
2021-07-17 18:48:18 -0400

    class Methods

---
+++ 17.Data-Structures-Intro/108.Class-Methods.js
@@ -0,0 +1,17 @@
+class Form {
+    constructor(date, month, year){
+        this.date = date;
+        this.month = month;
+        this.year = year;
+    }
+    validate(){
+
+    }
+    print(){
+
+    }
+    submit(){
+
+    }
+}
+
