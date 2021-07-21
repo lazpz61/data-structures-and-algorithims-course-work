001f10289e68e4997fd36a82f1d879d3152a4c00
LazaroMPerez <lazpz61@gmail.com>
2021-07-21 00:06:48 -0400

    get solution

---
+++ 18.Singly-Linked-Lists/120.Get-Solution.js
@@ -0,0 +1,11 @@
+/*
+
+- The function should accept an index.
+- If the index is less than zero or greater than or equal to the length of the list, return null.
+- Loop through the list until you reach the index and return the node at that specific index. 
+
+*/
+
+
+
+
