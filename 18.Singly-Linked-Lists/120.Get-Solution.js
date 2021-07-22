212bf2b9dda2f51771b8d47bce45b0a2c4331d1f
LazaroMPerez <lazpz61@gmail.com>
2021-07-21 23:19:27 -0400

    get solutions

--- 18.Singly-Linked-Lists/120.Get-Solution.js
+++ 18.Singly-Linked-Lists/120.Get-Solution.js
@@ -7,5 +7,41 @@
 */
 
 
+class Node{
+    constructor(value){
+        this.value = value,
+        this.next = null;
+    }
+}
 
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+        push(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while(counter != index){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+}
 
